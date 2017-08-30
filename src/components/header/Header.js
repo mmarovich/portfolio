4f0c6cd1e2e82b87ca5000f22ccd57d7e3f0c77e
mmarovich <mmarovich@gmail.com>
2017-08-30 15:55:51 -0500

    fixed menu popup position

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
         vertical: 'bottom',
       },
       targetOrigin: {
-        horizontal: 'right',
+        horizontal: 'middle',
         vertical: 'top',
       },
       width: ''
